91905a9a838baf5ca59571ecadfd052a477c017b
zipeng <1437966391@qq.com>
2024-05-06 21:03:54 +0800

    feat: init dts

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,14 @@
  * @Author: zipeng 1437966391@qq.com
  * @Date: 2024-05-04 16:41:43
  * @LastEditors: zipeng 1437966391@qq.com
- * @LastEditTime: 2024-05-04 16:42:49
+ * @LastEditTime: 2024-05-06 21:02:56
  * @FilePath: \web-track\rollup.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import fs from "fs";
 import path from "path";
 import typescript from "rollup-plugin-typescript2";
+import dts from "rollup-plugin-dts"
 const packagesDir = path.resolve(__dirname, "packages");
 const packageFiles = fs.readdirSync(packagesDir);
 function output(path) {
@@ -16,6 +17,16 @@ function output(path) {
     {
       input: [`./packages/${path}/src/index.ts`],
       output: [
+        {
+          file: `./packages/${path}/dist/index.cjs.js`,
+          format: 'cjs',
+          sourcemap: true,
+        },
+        {
+          file: `./packages/${path}/dist/index.esm.js`,
+          format: 'esm',
+          sourcemap: true,
+        },
         {
           file: `./packages/${path}/dist/index.js`,
           format: "umd",
@@ -33,6 +44,15 @@ function output(path) {
           useTsconfigDeclarationDir: true,
         }),
       ],
+    },
+    {
+      input: `./packages/${path}/src/index.ts`,
+      output: [
+        { file: `./packages/${path}/dist/index.cjs.d.ts`, format: 'cjs' },
+        { file: `./packages/${path}/dist/index.esm.d.ts`, format: 'esm' },
+        { file: `./packages/${path}/dist/index.d.ts`, format: 'umd' },
+      ],
+      plugins: [dts()],
     },
   ];
 }
